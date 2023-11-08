53d8bc2774350e59f120756475d52e9260ec8580
alejomj19971 <amueraja@cesde.net>
2023-11-07 22:23:00 -0500

    si

--- App.js
+++ App.js
@@ -10,7 +10,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <stack.Navigator 
-      initialRouteName="">
+      >
 
       <stack.Screen
         name="Registro Usuario"
