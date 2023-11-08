88c487d7b6bb369df13770014c87f4c384115e3a
alejomj19971 <amueraja@cesde.net>
2023-11-07 22:18:08 -0500

    Subiendo vistas

---
+++ assets/styles/allstyles.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: "center",
+      justifyContent: "center",
+      backgroundColor:  '#D1C4E9'
+    },
+    image: {
+      flex: 1,
+      justifyContent: 'center',
+    },
+  });
+
+  export {styles}
